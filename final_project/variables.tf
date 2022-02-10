b33aef5058ac01755b46ebd20ccfe69f1ddde2b8
Taryraiev Oleksandr <alezandergame@gmail.com>
2022-02-10 13:35:31 +0200

    add final project not final

---
+++ final_project/variables.tf
@@ -0,0 +1,25 @@
+# Configure the AWS Provider
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+# script to select last focal version of Ubuntu
+
+data "aws_ami" "latest_ubuntu" {
+  owners = ["099720109477"]
+  most_recent = true
+  filter {
+    name = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-*-amd64-server-*"]
+  }
+}
